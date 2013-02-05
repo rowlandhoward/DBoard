f0d9562c37fd5649e4ee85f8527e922a82d5648a
johnny joe <muradio72@gmail.com>
2013-02-05 03:26:33 -0500

    Created app

---
+++ wboard/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wboard::Application.config.secret_token = '9c5ad82c0ce70b361e26f58852e4eb7cd7eb5fd2bf7de4a77a604ea8674d646f30d111f5b7e0ffd77dbdd6bcc743735854c6ca29b34322cef22c628d1ae6c8e3'
